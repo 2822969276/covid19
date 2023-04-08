52860d696e208661e8cb478669040eb932f84237
zhenshuai <2822969276@qq.com>
2023-04-08 18:08:47 +0800

    Jsoup的获取和操作Document中的元素

--- crawler/src/main/java/cn/itcast/JsoupTest.java
+++ crawler/src/main/java/cn/itcast/JsoupTest.java
@@ -4,6 +4,7 @@ import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 
 import java.io.File;
@@ -14,6 +15,7 @@ import java.net.URL;
  * Desc：使用Jsoup实现页面解析
  */
 public class JsoupTest {
+    //初识Jsoup及其获取Document的几种不同的方法，jsoup.html为本地测试文件
     @Test
     public void testGetDocument() throws IOException {
         //方法一：使用connect方法直接连接网址获取Document
@@ -29,5 +31,43 @@ public class JsoupTest {
         Element titleElement = doc.getElementsByTag("title").first();
         String tital = titleElement.text();
         System.out.println(tital);
+    }
+    //通过Jsoup获取Document中的元素
+    @Test
+    public void testGetElement() throws IOException {
+        Document doc = Jsoup.parse(new File("jsoup.html"), "UTF-8");
+//        System.out.println(doc);
+
+        //根据标签ID获取元素getElementById
+        Element element = doc.getElementById("city_bj");
+        System.out.println(element.text());
+
+        //根据标签获取元素
+        Elements elements = doc.getElementsByTag("title");
+        System.out.println(elements.first().text());
+
+        //根据class获取元素
+        Elements elementsByClass = doc.getElementsByClass("s_name");
+        System.out.println(elementsByClass.last().text());
+
+        //根据属性名称获取元素
+        System.out.println(doc.getElementsByAttribute("abc").first().text());
+    }
+
+    //Jsoup的操作Document中的元素
+    @Test
+    public void testElementOperator() throws IOException {
+        Document doc = Jsoup.parse(new File("jsoup.html"), "UTF-8");
+        Element element = doc.getElementsByAttributeValue("class", "city_con").first();
+        //获取元素中的id
+        System.out.println(element.id());
+        //获取元素中的classname
+        System.out.println(element.className());
+        //获取元素中的属性值
+        System.out.println(element.attr("id"));
+        //从元素中获取所有的属性
+        System.out.println(element.attributes().toString());
+        //获取元素中的文本内容
+        System.out.println(element.text());
     }
 }
