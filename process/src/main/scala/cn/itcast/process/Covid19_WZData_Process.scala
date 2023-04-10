f91f3e2284108ed428610a51439d61da15b91514
zhenshuai <2822969276@qq.com>
2023-04-10 17:29:50 +0800

    手动提交偏移量

--- process/src/main/scala/cn/itcast/process/Covid19_WZData_Process.scala
+++ process/src/main/scala/cn/itcast/process/Covid19_WZData_Process.scala
@@ -3,7 +3,7 @@ package cn.itcast.process
 import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.streaming.dstream.{DStream, InputDStream}
-import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+import org.apache.spark.streaming.kafka010.{CanCommitOffsets, ConsumerStrategies, HasOffsetRanges, KafkaUtils, LocationStrategies, OffsetRange}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
@@ -56,7 +56,7 @@ object Covid19_WZData_Process {
       //earliest表示如果记录了偏移量则从记录的位置开始消费，如果没有记录则从最开始/最早的位置开始消费
       //none表示如果记录了偏移量则从记录的位置开始消费，如果没有记录则报错
       ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> "latest", //偏移量重置位置
-      ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG -> (true: lang.Boolean), //是否自动提交偏移量
+      ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG -> (false: lang.Boolean), //是否自动提交偏移量
       ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
       ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer]
     )
@@ -67,9 +67,31 @@ object Covid19_WZData_Process {
       LocationStrategies.PreferConsistent,
       ConsumerStrategies.Subscribe[String, String](topics, kafkaParams)
     )
-    //4.实时处理数据
-    val valueDS: DStream[String] = kafkaDS.map(_.value())
-    valueDS.print()
+    //4.实时处理数据并手动提交偏移量
+//    val valueDS: DStream[String] = kafkaDS.map(_.value())
+//    valueDS.print()
+    //我们要手动提交偏移量，那么意味着，消费了一批数据就应该提交一次偏移量
+    //在SparkStreaming中数据抽象为DStream，DStream的底层其实也就是RDD，也就是每一批次的数据
+    //所以接下来我们应该对DStream中的RDD进行处理
+    kafkaDS.foreachRDD(
+      rdd => {
+        if(rdd.count()>0) {//如果该RDD中有数据，则处理
+          rdd.foreach(
+            record => {
+              print("从Kafka中消费到的每一条消息："+record)
+              //获取偏移量
+              //使用Spark-streaming-kafka-0-10封装好的API来存放偏移量并提交
+              val offsets: Array[OffsetRange] = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+              for (o <- offsets) {
+                print(s"topic=${o.topic},partition=${o.partition},fromOffset=${o.fromOffset},until=${o.untilOffset}")
+              }
+              //手动提交偏移量到Kafka的默认主题：__consumer__offsets中，如果开启了Checkpoint还会提交到checkpoint中
+              kafkaDS.asInstanceOf[CanCommitOffsets].commitAsync(offsets)
+            }
+          )
+        }
+      }
+    )
     //5.将处理分析的结果存入到Mysql
 
     //6.开启SparkStreaming任务并等待结束
